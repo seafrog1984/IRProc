ec57cb64da299aef9a55b776c8ed232d0584b616
seafrog1984@hotmail.com <ncut3478186789>
2017-05-03 14:34:43 +0800

    v1.2 open-new

--- IRProc/IMGView.cpp
+++ IRProc/IMGView.cpp
@@ -61,29 +61,122 @@ void CIMGView::Dump(CDumpContext& dc) const
 void CIMGView::OnBnClickedOpen()
 {
 	// TODO:  在此添加控件通知处理程序代码
-	CFileDialog dlg(TRUE, _T("*.bmp"), NULL, OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY, _T("image files (*.bmp; *.jpg) |*.bmp; *.jpg | All Files (*.*) |*.*||"), NULL);                            // 选项图片的约定     
-	dlg.m_ofn.lpstrTitle = _T("Open Image");// 打开文件对话框的标题名     
-	if (dlg.DoModal() != IDOK)             // 判断是否获得图片         
-		return;
-	CString m_path = dlg.GetPathName();
+	//
+	//CFileDialog dlg(TRUE, _T("*.bmp"), NULL, OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY, _T("image files (*.bmp; *.jpg) |*.bmp; *.jpg | All Files (*.*) |*.*||"), NULL);                            // 选项图片的约定     
+	//dlg.m_ofn.lpstrTitle = _T("Open Image");// 打开文件对话框的标题名     
+	//if (dlg.DoModal() != IDOK)             // 判断是否获得图片         
+	//	return;
+	//CString m_path = dlg.GetPathName();
 
-	Mat src = imread((LPSTR)(LPCSTR)m_path);
+	//Mat src = imread((LPSTR)(LPCSTR)m_path);
+	#define F_TO_C(x)  ((((x)-32.0) * 5.0) / 9.0)
+	#define C_TO_F(x)    ((32.0) + ((x) * (9.0/5.0)))
+	unsigned short *tmp= (unsigned short*)malloc(320*240*sizeof(short));
+	long width=320, height=240;
+	ifstream fin("b.dat");
 
-	Mat img1;
-	CRect rect;
-	GetDlgItem(IDC_PIC)->GetClientRect(&rect);
-
-	resize(src, img1, Size(rect.Width(), rect.Height()), 0, 0);
-
-	/*ofstream out("F:\\img.txt", ios::out);
-
-	for (int i = 0; i < src.rows; i++)
+	for (int i = 0; i < width*height; i++)
 	{
-		for (int j = 0; j < src.cols; j++)
+		fin>> *(tmp+i);
+		if (*(tmp+i) == ' ')
+			i--;
+	}
+	
+	fin.close();
+
+	Mat src(height, width, CV_8UC1, tmp);
+	namedWindow("src");
+	imshow("src", src);
+	Mat img;
+	//	img.create(height, width, CV_16UC1, tmp);
+	src.copyTo(img);
+	//	cvtColor(src,img , CV_BGR2GRAY);
+
+	Mat dst, g_src(img), g_dst;
+	dst.create(img.size(), CV_8UC3);
+	g_dst.create(img.size(), CV_8UC1);
+	//cvtColor(img, g_src, CV_BGR2GRAY);
+
+	if (tmp)
+	{
+		
+		
+		//	SetDlgItemText(IDC_CENTER_TEMP, buf);
+
+		// highly non-optimal conversion from 16-bit to 8-bit
+
+		// Values are in degrees C * 100.0
+		// So, 1 deg C would == 100
+		// My mind doesn't work well in deg C so I have a F_TO_C conversion
+		//
+		// topvalue represents the highest temperature value we want to display
+		// bottomvalue represents the lowest temperature value we want to display
+		//
+
+		bool auto_range = true; // set to false to use manual range...
+
+		// Manual range values
+		float topvalue = (int)(F_TO_C(130) * 100.0);
+		float bottomvalue = (int)(F_TO_C(50) * 100.0);
+
+
+		int i, end, col, row;
+		float value;
+		unsigned short *src = (unsigned short *)tmp;
+		//unsigned char  *dest = (unsigned char  *)m_ImageData8;
+		unsigned char displayValue;
+		end = width * height;  // image size...
+
+		if (auto_range)
 		{
-			out << i << "," << j << src.at<uchar>(i, j) << endl;
+			bottomvalue = 99999;
+			topvalue = -99999;
+			for (i = 0; i < end; i++)
+			{
+				value = src[i];
+				bottomvalue = min(bottomvalue, value);
+				topvalue = max(topvalue, value);
+			}
 		}
-	}*/
-	//载入后先显示
-	imshow("view", img1);
+
+		float range = topvalue - bottomvalue;
+
+		if (range != 0)
+		{
+
+			for (i = 0; i < end; i++)
+			{
+				value = *src++;
+				// Limit upper + lower values
+				if (value < bottomvalue) value = bottomvalue;
+				if (value > topvalue)    value = topvalue;
+				// Scale to 0..255 for display
+				displayValue = ((value - bottomvalue) * 255) / range;
+
+				row = i / width;
+				col = i%width;
+				g_dst.at<uchar>(row, col) = displayValue;
+			}
+		}
+	}
+	//int i, j;
+	//for (i = 0; i < dst.rows; i++)
+	//{
+	//	for (j = 0; j < dst.cols; j++)
+	//	{
+	//		int tmp = g_src.at<uchar>(i, j);
+
+	//		dst.at<Vec3b>(i, j)[0] = 101.2 - 116.2*cos(tmp*0.08655) + 91.93*sin(tmp*0.08592);
+	//		dst.at<Vec3b>(i, j)[1] = 150.9 - 110.9*cos(tmp*0.08457) - 97.33*sin(tmp*0.08457);
+	//		dst.at<Vec3b>(i, j)[2] = 125.3 + 59.93*cos(tmp*0.04896) - 130.2*sin(tmp*0.04896);
+
+
+	//	}
+	//}
+
+	namedWindow("test");
+
+	imshow("test", g_dst);
+
+
 }
